9b60ac0736c9031a46e3a754b460d5244784be82
khws4v1 <khws4v1@gmail.com>
2019-07-06 23:30:17 +0900

    Modify Mastodon version

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def flags
-      ''
+      'ef67_myhome_cx'
     end
 
     def to_a
@@ -33,16 +33,16 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY') { 'tootsuite/mastodon' }
+      ENV.fetch('GITHUB_REPOSITORY') { 'khws4v1/ef67.myhome.cx' }
     end
 
     def source_base_url
-      ENV.fetch('SOURCE_BASE_URL') { "https://github.com/#{repository}" }
+      ENV.fetch('SOURCE_BASE_URL') { "https://khws4v1.myhome.cx/gitlab/#{repository}" }
     end
 
     # specify git tag or commit hash here
     def source_tag
-      ENV.fetch('SOURCE_TAG') { nil }
+      ENV.fetch('SOURCE_TAG') { 'ef67.myhome.cx' }
     end
 
     def source_url
